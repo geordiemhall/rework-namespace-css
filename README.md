f3e2a57496a94530706fb6483489bb19df19824b
Geordie Hall <geordiemhall@gmail.com>
2016-08-26 09:58:08 +1000

    Added ignoreUnderscored option
    Version bump to 0.1.4

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Rework Namespace CSS
 ======
 
-Namespacing plugin for Rework.
+A simple namespacing plugin for Rework.
 
-Allows for CSS to be namespaced by selector, and with class prefixes.
+Allows for CSS to be namespaced by selector, and with class prefixes. 
 
 
 ## Options
@@ -20,53 +20,88 @@ Pass in an options object to configure the plugin. Possible options:
 
 `namespaceBody`: whether `body` should be converted to `.body`. Useful if complete namespacing is required. Defaults to `true`
 
+`ignoreUnderscored`: whether selectors that begin with an underscore should have the underscore removed and not be namespaced. Defaults to `true`. Useful for selectors that need to break out of namespacing. Eg. `._reset {}` won't get transformed into `.namespace ._reset {}`, but as .`reset{}` instead
+
 
 ## Example
 
-The following gulp snippet
+Note: Rework is fairly old now so the following may no longer work.
 
-    var rework = require('rework')
-    rework.namespace = require('rework-namespace-css')
-    
-    ...
-    
-    return gulp.src('src/styles/index.css')
-        .pipe(rework(rework.namespace({ selector: '.gmh', class: 'gmh-' })))
-        .pipe(gulp.dest('dist/styles'))
-
-Will turn
-
-    html {
-        background: red;
-    }    
-    
-    body {
-        color: blue;
+Grunt snippet to show the general idea:
+```js
+var reworkNamespace = require('rework-namespace-css')
+grunt.initConfig({
+    rework: {
+        options: {
+            use: [
+                reworkNamespace({
+                    selector: '.gmh'
+                })
+            ]
+        },
+        index: {
+            'dist/styles/core.css': 'dist/styles/core-namespaced.css'
+        }
     }
-    
-    .button {
-        border: 1px solid black;
-    }
-    
-    a.button {
-        border-color: red;
-    }
+})
+```
 
-Into
 
-    .html.gmh {
-        background: red;
-    }    
+Gulp snippet to show the general idea (untested, may not work):
+```js
+var rework = require('gulp-rework')
+rework.namespace = require('rework-namespace-css')
 
-    .gmh .body {
-        color: blue;
-    }
+// ...
 
-    .gmh .gmh-button {
-        border: 1px solid black;
-    }
+return gulp.src('src/styles/index.css')
+    .pipe(rework(rework.namespace({ selector: '.gmh', class: 'gmh-' })))
+    .pipe(gulp.dest('dist/styles'))
+```
 
-    .gmh a.gmh-button {
-        border-color: red;
-    }
+Will take the following CSS:
+```css
+html {
+    background: red;
+}    
 
+body {
+    color: blue;
+}
+
+.button {
+    border: 1px solid black;
+}
+
+a.button {
+    border-color: red;
+}
+
+._ignore-this {
+    box-sizing: border-box;
+}
+```
+
+And turn it into:
+```css
+.html.gmh {
+    background: red;
+}    
+
+.gmh .body {
+    color: blue;
+}
+
+.gmh .gmh-button {
+    border: 1px solid black;
+}
+
+.gmh a.gmh-button {
+    border-color: red;
+}
+
+.ignore-this {
+    box-sizing: border-box;
+}
+
+```
--- index.js
+++ index.js
@@ -4,49 +4,40 @@
 // Built on Rework. Can prefix selectors and classes.
 // Options:
 // 
-// 		class: all classes will have this string prepended to them
-// 		selector: all selectors will have this string prepended to them (except html) 
-// 		namespaceHtml: should html have prefix appended to it directly? or with a space (eg. descendant)?
-// 		namespaceBody: should body selectors also have a non-body prefixed version 
-// 		root: the element to treat as the highest level. Defaults to html
-
-// TODO @geordiemhall: Implement keyframes option 
+// 		class: 			all classes will have this string prepended to them
+// 		selector: 		all selectors will have this string prepended to them (except html) 
+// 		namespaceHtml: 	should html have prefix appended to it directly? or with a space (eg. descendant)?
+// 		namespaceBody: 	should body selectors also have a non-body prefixed version 
+// 		root: 			the element to treat as the highest level. Defaults to html
+// 		duplicateBody: 	body should also have a .root.body set (in addition to .)
+// 		ignoreUnderscored: should any selectors that start with an underscore be ignored and their underscore removed (allows to break out of namespace)
 
 var walk = require('rework-walk');
 
-
-module.exports = function prefix(options) {
-
+module.exports = function prefix(options){
 	var opts = options || {}
 	var regBody = /(?:\s+|^)body(?:\s+|$|\.|\:){1}/
 	var regHtml = /(?:\s+|^)html(?:\s+|$|\.|\:){1}/
 
-	return function prefix(style) {
-		
+	return function prefix(style){
 		function prefixSelector(selector){
-
 			var sel = selector
 
 			if (opts.namespaceBody !== false){
-
 				// Replace body with .body
 				sel = sel.replace(regBody, function(s){ 
 					return s.replace('body', '.body')
 				})
-
 			}
 
 			if (opts.namespaceHtml !== false){
-
 				// Replace html with .html
 				sel = sel.replace(regHtml, function(s){ 
 					return s.replace('html', '.html')
 				})
-
 			}
 
 			if (opts.selector){
-
 				var root = opts.root || 'html'
 
 				// TODO: This method would only replace the first instance of root,
@@ -60,31 +51,38 @@ module.exports = function prefix(options) {
 					sel = opts.selector + ' ' + sel
 				}
 
+				if (opts.duplicateBody !== false && opts.namespaceBody !== false){
+					if (sel.indexOf(opts.selector + ' .body') >= 0){
+						sel = [ sel, sel.replace(' .body', '.body') ].join(', ')
+					}
+				}
+			}
+
+			if (opts.ignoreUnderscored !== false){
+				// Any classes with an underscore in them are treated raw, with the underscore removed
+				// eg. ._reset {} won't get prefixed as .namespace ._reset {}, but as .reset{}
+				if (sel.indexOf('._') >= 0){
+					sel = selector.replace(/\._/g, '.') // first instance is purposeful for now
+				}
 			}
 
 			return sel
-
 		}
 
 		function prefixClasses(selector){
-
 			return selector.split('.').join('.'+(opts.class || ''))
-
 		}
 
 		// Walk all styles
-		walk(style, function(rule, node) {
-
+		walk(style, function(rule, node){
 			// Don't touch keyframes or font-face
-			if (!rule.selectors || rule.selectors.toString().indexOf('@') >= 0) 
+			if (!rule.selectors || rule.selectors.toString().indexOf('@') >= 0){
 				return rule;
+			}
 
-			rule.selectors = rule.selectors.map(function(selector) {
+			rule.selectors = rule.selectors.map(function(selector){
 				return prefixSelector(prefixClasses(selector))
 			});
-
 		});
-
 	}
-
-};
+}
